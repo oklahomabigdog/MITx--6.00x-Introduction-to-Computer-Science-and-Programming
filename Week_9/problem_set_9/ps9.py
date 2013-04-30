a989b1e6ff5d62802103d873dda1c89a0fa25639
Doane Harrington <doane.harrington@adfitech.com>
2013-04-30 11:42:19 -0500

    Added Week 9 and 10.

---
+++ Week_9/problem_set_9/ps9.py
@@ -0,0 +1,48 @@
+# 6.00 Problem Set 9
+
+import numpy
+import random
+import pylab
+from ps8b import *
+
+#
+# PROBLEM 1
+#        
+def simulationDelayedTreatment(numTrials):
+    """
+    Runs simulations and make histograms for problem 1.
+
+    Runs numTrials simulations to show the relationship between delayed
+    treatment and patient outcome using a histogram.
+
+    Histograms of final total virus populations are displayed for delays of 300,
+    150, 75, 0 timesteps (followed by an additional 150 timesteps of
+    simulation).
+
+    numTrials: number of simulation runs to execute (an integer)
+    """
+    
+    # TODO
+
+
+
+
+
+
+#
+# PROBLEM 2
+#
+def simulationTwoDrugsDelayedTreatment(numTrials):
+    """
+    Runs simulations and make histograms for problem 2.
+
+    Runs numTrials simulations to show the relationship between administration
+    of multiple drugs and patient outcome.
+
+    Histograms of final total virus populations are displayed for lag times of
+    300, 150, 75, 0 timesteps between adding drugs (followed by an additional
+    150 timesteps of simulation).
+
+    numTrials: number of simulation runs to execute (an integer)
+    """
+    # TODO
